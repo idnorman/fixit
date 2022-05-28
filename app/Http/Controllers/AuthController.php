45a100552a009330255f31958f1cee0dc9392143
Norman <norman.primary@gmail.com>
2022-05-28 14:20:18 +0700

    first commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\User;
+use App\Traits\ApiResponser;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    use ApiResponser;
+
+    public function register(Request $request){
+
+        $attr = $request->validate([   
+            'name'      => 'required|string|max:255',
+            'email'     => 'required|string|email|unique:users,email',
+            'password'  => 'required|string|min:6|confirmed'
+        ]);
+
+        $user = User::create([
+            'name'      => $attr['name'],
+            'password'  => Hash::make($attr['password']),
+            'email'     => $attr['email']
+        ]);
+
+        return $this->success([
+            'token'     => $user->createToken('auth_token')->plainTextToken
+        ]);
+
+    }
+
+    public function login(Request $request){
+
+        $attr = $request->validate([
+            'email'     => 'required|string|email',
+            'password'  => 'required|string|min:6'
+        ]);
+        
+        if (!Auth::attempt($attr)) {
+            return $this->error('Credentials not match', 401);
+        }
+
+        return $this->success([
+            'token'     => auth()->user()->createToken('auth_token')->plainTextToken
+        ]);
+
+    }
+
+    public function logout()
+    {
+        auth()->user()->tokens()->delete();
+
+        return $this->success([], 'Token revoked');
+
+    }
+
+}
